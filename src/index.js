0b2188e565e7bfada44a2283cc8341575fea2665
kallen312 <73615486+kallen312@users.noreply.github.com>
2022-04-06 18:10:39 -0400

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import "./styles.css";
+
+//Create a loop that logs each array element to the console.
+//Hint: Remember, to log something to the console, you'll need to use console.log().;
+let fruits = ["grape", "banana", "orange", "apple", "strawberry"];
+
+fruits.map((fruit) => console.log(fruit));
